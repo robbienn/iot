d269ec1649ba863d3cfc8f3e65ff40e4af265add
Robert Nemecek <robert.nemecek@volny.cz>
2018-12-19 11:20:54 +0100

    Adding wemod D1 R2 dir + strip code

--- RPi.py/lcd_2004_plus_DHT22_plus_GYBM280_02.py
+++ RPi.py/lcd_2004_plus_DHT22_plus_GYBM280_02.py
@@ -3,6 +3,15 @@ import RPi_I2C_driver
 from time import *
 import Adafruit_DHT
 from Adafruit_BME280 import *
+import httplib, urllib
+
+
+sleep_seconds = 600
+
+
+channel_id = 604642
+write_key  = "SMYAN1ZW2XJJTHUK"
+
 
 # Termometer DHT22 ~ AM2302
 dht_sensor = Adafruit_DHT.DHT22
@@ -21,10 +30,15 @@ mylcd.lcd_clear()
 while(True):
 
     humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, dht_pin)    
+    humidity = round(humidity, 2)
+    temperature = round(temperature, 2)
 
     temperature2 = pressure_sensor.read_temperature()
     pressure = pressure_sensor.read_pressure() / 100
     #humidity2 = pressure_sensor.read_humidity()
+
+    temperature2 = round(temperature2, 2)
+    pressure = round(pressure, 4)
 
     print '-------------------------------'
 
@@ -48,9 +62,29 @@ while(True):
         mylcd.lcd_display_string(" Teplota2: " + temp2, 4)
     else:
         print('Failed to get reading. Try again!')
+        next
+
+    print 'temperature   = {}'.format(temperature)
+    print 'humidity      = {}'.format(humidity)
+    print 'pressure      = {}'.format(pressure)
+
+    params = urllib.urlencode({'field1': temperature, 'field2': humidity, 'field3': pressure, 'key': write_key})
+    
+    headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+    conn = httplib.HTTPConnection("api.thingspeak.com:80")                
+    try:
+            conn.request("POST", "/update", params, headers)
+            response = conn.getresponse()
+            print temp
+            print response.status, response.reason
+            data = response.read()
+            conn.close()
+    except:
+            print "connection failed"
 
 
-    sleep(15) # 15 sec delay
+    print('Sleeping...')
+    sleep(sleep_seconds)
 
     mylcd.lcd_clear()
 
